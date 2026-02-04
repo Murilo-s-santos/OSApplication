0a43a345b18dc2338f428d8054360a6abca46a96
murilo <mumurilosiq@gmail.com>
2026-02-04 17:13:39 -0300

    Inciio da fase 1

---
+++ src/main/java/br/dev/murilo/OSApiApplication/domain/model/Cliente.java
@@ -0,0 +1,26 @@
+package br.dev.murilo.OSApiApplication.domain.model;
+
+public class Cliente 
+{
+    // Atributos
+    private long id;
+    private String nome;
+    private String email; 
+    private String fone;
+    
+    // Construtores
+    public Cliente()
+    {
+        
+    }
+    public Cliente(long id, String nome, String email, String fone)
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
