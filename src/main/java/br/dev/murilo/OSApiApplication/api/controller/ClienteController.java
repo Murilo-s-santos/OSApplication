a55edb85396df45fa33f2ce9e87bd8e5ea3f7181
murilo <mumurilosiq@gmail.com>
2026-02-20 11:25:53 -0300

    Alterações realizadas: . Criação da classe ProblemaException; . Melhoria na interface do usuário.

--- src/main/java/br/dev/murilo/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/dev/murilo/OSApiApplication/api/controller/ClienteController.java
@@ -4,10 +4,20 @@ import br.dev.murilo.OSApiApplication.domain.model.Cliente;
 import br.dev.murilo.OSApiApplication.domain.repository.ClienteRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,7 +30,59 @@ public class ClienteController
     public List<Cliente> listas()
     {
         //return clienteRepository.findAll();
-        return clienteRepository.findByNome("ednhfuoehfgurg");
+        return clienteRepository.findByNome("KGe");
         // return clienteRepository.findByNomeContaining("Silva");
     }
+    
+    @GetMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteID)
+    {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteID);
+        
+        if (cliente.isPresent())
+        {
+            return ResponseEntity.ok(cliente.get());
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente)
+    {
+        return clienteRepository.save(cliente);
+    }
+    
+    @PutMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteID,
+                                             @RequestBody Cliente cliente)
+    {
+        // Verifica se oc liente existe
+        if (!clienteRepository.existsById(clienteID))
+        {
+            return ResponseEntity.notFound().build();
+        }
+        
+        cliente.setId(clienteID);
+        cliente = clienteRepository.save(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+    
+    @DeleteMapping("/clientes/{clienteID}")
+    public ResponseEntity<Void> excluir(@PathVariable Long clienteID)
+    {
+        // Verifica se o cliente existe ou não
+        if(!clienteRepository.existsById(clienteID))
+        {
+            return ResponseEntity.notFound().build();
+        }
+        
+        clienteRepository.deleteById(clienteID);
+        return ResponseEntity.noContent().build();
+    }
+    
+    
 }
