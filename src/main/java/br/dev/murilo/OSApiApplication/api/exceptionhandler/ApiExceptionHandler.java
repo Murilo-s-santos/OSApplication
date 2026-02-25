5fb7fc13158af0b084ee97b44cdbca32245fedad
murilo <mumurilosiq@gmail.com>
2026-02-25 16:11:17 -0300

    Alteraçõres realizadas: . Término da fase 5.

--- src/main/java/br/dev/murilo/OSApiApplication/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/dev/murilo/OSApiApplication/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,19 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler 
 {
+    
+    
+    @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
         MethodArgumentNotValidException ex,
         HttpHeaders headers,
-        HttpStatus status,
+        HttpStatusCode status,
         WebRequest request) {
         
         ProblemaException problema = new ProblemaException();
@@ -39,6 +45,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler
         
         return super.handleExceptionInternal(ex, problema, headers, status, request);
     }
+
     
 }
     
